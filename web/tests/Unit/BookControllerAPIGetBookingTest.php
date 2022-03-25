f7746258c8ef3febe1e201692db908e57847a01d
parinz1234 <parinya.ons@gmail.com>
2022-03-25 21:42:05 +0700

    test: example test case

--- web/tests/Unit/BookControllerAPIGetBookingTest.php
+++ web/tests/Unit/BookControllerAPIGetBookingTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit;
 
 // use PHPUnit\Framework\TestCase;
 use Tests\TestCase;
+use DB;
+use App\Http\Controllers\api\BookController;
 
 class BookControllerAPIGetBookingTest extends TestCase
 {
@@ -12,10 +14,50 @@ class BookControllerAPIGetBookingTest extends TestCase
      *
      * @return void
      */
-    public function test_get_booking_response_should_contains_bookings()
+    public function test_get_booking_response_should_contains_bookings_call_get_method()
     {
+        $books = [
+            (object)[
+                "id" => 1,
+                "name" => "name",
+                "image_path" => "image_path",
+                "created_at" => null,
+                "updated_at" => null
+            ]
+        ];
+
+        DB::shouldReceive('table->get')
+            ->once()
+            ->andReturn($books);
+
         $response = $this->get('/api/books');
-        dd($response->json());
+
         $response->assertStatus(200);
+        $response->assertJson([
+            'bookings' => json_encode($books),
+        ]);
+    }
+
+    public function test_get_booking_response_should_contains_bookings_call_ctrl_func()
+    {
+        $books = [
+            (object)[
+                "id" => 1,
+                "name" => "name",
+                "image_path" => "image_path",
+                "created_at" => null,
+                "updated_at" => null
+            ]
+        ];
+
+        DB::shouldReceive('table->get')
+            ->once()
+            ->andReturn($books);
+
+        $controller = new BookController();
+        $response = $controller->get_booking();
+
+        $this->assertEquals($response->status(), 200);
+        $this->assertJson($response->content());
     }
 }
